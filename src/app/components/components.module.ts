87fc3259f2b61bd7b1adbd0d7fff388fdfc93de9
Zine Samy <zine.samy@gmail.com>
2018-08-02 16:38:53 +0200

    packaging ok

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  grabCursor: true
+};
+
+
 import {
   SwitchConsoleActionsComponent,
   ImageConsoleActionComponent,
@@ -28,13 +39,17 @@ const PAGES_DIRECTIVES = [
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    SwiperModule
   ],
   declarations: [
     ...PAGES_DIRECTIVES
   ],
   providers: [ 
-
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
   ],
   exports: [
      ...PAGES_DIRECTIVES
