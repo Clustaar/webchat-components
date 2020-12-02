1ef345dacc8c12b60bf93f90c2cf363dced91702
Zine Samy <zine.samy@gmail.com>
2020-12-02 15:18:38 +0100

    update core and cli

--- projects/clustaar-webchat-components/src/lib/agent-reply/agent-reply.component.spec.ts
+++ projects/clustaar-webchat-components/src/lib/agent-reply/agent-reply.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AgentReplyComponent } from './agent-reply.component';
 
@@ -6,7 +6,7 @@ describe('AgentReplyComponent', () => {
   let component: AgentReplyComponent;
   let fixture: ComponentFixture<AgentReplyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AgentReplyComponent ]
     })
--- projects/clustaar-webchat-components/src/lib/send-js-event/send-js-event.component.spec.ts
+++ projects/clustaar-webchat-components/src/lib/send-js-event/send-js-event.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SendJsEventComponent } from './send-js-event.component';
 
@@ -6,7 +6,7 @@ describe('SendJsEventComponent', () => {
   let component: SendJsEventComponent;
   let fixture: ComponentFixture<SendJsEventComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SendJsEventComponent ]
     })
