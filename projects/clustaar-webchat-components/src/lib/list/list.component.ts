385d259e709863c8a02eb863635509410d949217
DESKTOP-FP0SV88\loict <loic.tugaye@outlook.fr>
2022-07-01 10:09:09 +0200

    use angular material autocomplete

--- projects/clustaar-webchat-components/src/lib/list/list.component.ts
+++ projects/clustaar-webchat-components/src/lib/list/list.component.ts
@@ -14,42 +14,6 @@ export class ListComponent {
   @Input() userTextColor: string = '#2C3F59';
   @Output() onSuggestionClick = new EventEmitter<string>();
   filteredSections: Section[] = [];
-  stateCtrl = new FormControl('');
-  filteredStates: Observable<State[]>;
-
-  states: State[] = [
-    {
-      name: 'Arkansas',
-      population: '2.978M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
-    },
-    {
-      name: 'California',
-      population: '39.14M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
-    },
-    {
-      name: 'Florida',
-      population: '20.27M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
-    },
-    {
-      name: 'Texas',
-      population: '27.47M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
-    }
-  ];
-
-  constructor() {
-    /*this.filteredStates = this.stateCtrl.valueChanges.pipe(
-      startWith(''),
-      map(state => (state ? this._filterStates(state) : this.states.slice())),
-    );*/
-  }
 
   filter(inputValue: string): void {
     if (this.list.sections && inputValue != '') {
@@ -62,18 +26,12 @@ export class ListComponent {
             .includes(inputValue.toUpperCase().trim())
         );
       });
+    } else {
+      this.filteredSections = [];
     }
   }
 
-  sendSelectedValue(): void {
-    this.onSuggestionClick.emit('value');
-  }
-
-  private _filterStates(value: string): State[] {
-    const filterValue = value.toLowerCase();
-
-    return this.states.filter((state) =>
-      state.name.toLowerCase().includes(filterValue)
-    );
+  sendSelectedValue(choiceTitle: string): void {
+    this.onSuggestionClick.emit(choiceTitle);
   }
 }
