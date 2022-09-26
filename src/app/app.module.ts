de913a588390a266256e37aa9c870e9e1db952d2
Zine Samy <zine.samy@gmail.com>
2022-09-26 08:09:40 +0200

    zoneless demo and list auto open

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { routing } from './app.routing';
 
-import { DemoComponent } from './demo/demo.component';
 import { AppComponent } from './app.component';
 import { WebchatComponentsModule } from '../../projects/clustaar-webchat-components/src/lib/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,13 +9,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     WebchatComponentsModule,
-    routing,
     BrowserAnimationsModule
   ],
   providers: [],
